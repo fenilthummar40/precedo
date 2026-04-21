cda58eeab2a2a218d3a730ed08ae7067b23bf7c4
rhdholariya <rahuldholariya001@gmail.com>
2026-04-21 12:15:55 +0530

    change

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -62,54 +62,56 @@ function Footer() {
 
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-10'>
                         <div>
-                            <p className='text-gray-400 font-normal text-sm lg:text-lg line-clamp-2'>Contrary to popular belief, Lorem Ipsum is
+                            <p className='text-gray-400 font-normal text-sm lg:text-lg line-clamp-2'>Contrary to popular
+                                belief, Lorem Ipsum is
                                 not <br/> simply random text. It has
                                 roots.Conry to <br/> popular belief.</p>
 
                             <div className='flex items-center gap-5 mt-5'>
                                 <span
-                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-yellow-400 to-pink-500 cursor-pointer'><IconBrandTwitter/></span>
+                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-purple-600 to-pink-500 cursor-pointer'><IconBrandTwitter/></span>
                                 <span
-                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-yellow-400 to-pink-500 cursor-pointer'><IconBrandFacebook/></span>
+                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-purple-600 to-pink-500 cursor-pointer'><IconBrandFacebook/></span>
                                 <span
-                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-yellow-400 to-pink-500 cursor-pointer'><IconBrandInstagram/></span>
+                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-purple-600 to-pink-500 cursor-pointer'><IconBrandInstagram/></span>
                                 <span
-                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-yellow-400 to-pink-500 cursor-pointer'><IconBrandGit/></span>
+                                    className='border rounded-full border-gray-600 flex items-center justify-center p-2 text-white hover:bg-gradient-to-r from-purple-600 to-pink-500 cursor-pointer'><IconBrandGit/></span>
                             </div>
                         </div>
 
                         <div>
                             <ul className='flex items-center flex-wrap gap-4 md:gap-8'>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">Home</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">About Us</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">How it Works</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">Referral Program</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">Help Center</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">Security</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">FAQs</a></li>
                                 <li><a
-                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-yellow-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
+                                    className='text-white font-normal text-sm md:text-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 hover:bg-clip-text hover:text-transparent'
                                     href="">Avoiding Scams</a></li>
                             </ul>
                         </div>
                     </div>
 
                     <div className='mt-5 border border-gray-600 rounded-t-lg p-2 grid grid-cols-1 md:grid-cols-2 '>
-                        <div><p className='text-gray-400 font-normal text-sm md:text-lg'>© 2024 Precedo All Rights Reserved by site  </p></div>
+                        <div><p className='text-gray-400 font-normal text-sm md:text-lg'>© 2024 Precedo All Rights
+                            Reserved by site </p></div>
                         <div className='flex items-center justify-end gap-5'>
                             <p className='text-gray-400 font-normal text-sm md:text-lg'>Privacy Policy</p>
                             <p className='text-gray-400 font-normal text-sm md:text-lg'>Terms & Conditions</p>
